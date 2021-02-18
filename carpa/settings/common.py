3594b30fdf5570a775858f4d01055f138271d2f3
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-02-18 16:00:50 -0500

    changed timezone

--- carpa/settings/common.py
+++ carpa/settings/common.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/bogota'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
