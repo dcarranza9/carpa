7ad2ecb413ad1e2e23953568d64f1421b1b6e0c3
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-02-24 01:15:41 -0500

    removed fieldset tuple

--- main/admin.py
+++ main/admin.py
@@ -2,9 +2,4 @@ from django.contrib import admin
 
 
 class BaseModelAdmin(admin.ModelAdmin):
-    readonly_fields = ('creation_date', 'update_date')
-    fieldsets = (
-        ('Audit', {
-            'fields': ('creation_date', 'update_date'),
-        })
-    )
+    readonly_fields = ('creation_date', 'update_date',)
