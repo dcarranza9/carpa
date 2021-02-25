e3a0c65367e36d68832acdbacfe4e69707ab0163
Fabricio Caicedo <48930687+F4brii@users.noreply.github.com>
2021-02-24 22:36:33 -0500

    Driver and Vehicle models for sources application (#136)
    * Added driver model
    
    * Added driver model in admin
    
    * Added driver model migration
    
    * Added vehicle model
    
    * Added vehicle model in admin
    
    * Added vehicle model migration
    
    * Changed vehicle model
    
    * Changed vehicle model in admin
    
    * Changed vehicle model
    
    * Changed vehicle model in admin
    
    * added vehicle model migration
    
    * Changed vehicle model
    
    * Changed vehicle model in admin
    
    * Added vehicle model migration
    
    Co-authored-by: Fabricio <fabricio@pop-os.localdomain>

--- sources/admin.py
+++ sources/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.gis import admin as gis_admin
 from main.admin import BaseModelAdmin
-from sources.models import Parcel, ParcelOwner
+from sources.models import Parcel, ParcelOwner, Driver, Vehicle
 
 
 @admin.register(ParcelOwner)
@@ -44,3 +44,35 @@ class ParcelAdmin(gis_admin.OSMGeoAdmin):
     default_lat = 459132.20787
     default_lon = -8195052.12787
     default_zoom = 15
+
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    readonly_fields = BaseModelAdmin.readonly_fields
+    fieldsets = (
+        (None, {
+            'fields': (BaseModelAdmin.readonly_fields,)
+        }),
+        ('Person', {
+            'fields': ('name', 'address', 'email', 'phone', 'web')
+        }),
+    )
+    list_display = ('id', 'name', 'creation_date', 'update_date')
+    list_filter = ('creation_date',)
+
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    readonly_fields = BaseModelAdmin.readonly_fields
+    fieldsets = (
+        (None, {
+            'fields': (BaseModelAdmin.readonly_fields),
+        }),
+        ('Vehicle', {
+            'fields': ('plate', 'model', 'brand', 'details', 'driver')
+        })
+    )
+    list_display = ('id', 'plate', 'model', 'brand', 'details',
+                    'driver', 'creation_date', 'update_date')
+    list_filter = ('plate', 'model',
+                   'brand', 'driver', 'creation_date')
--- sources/models.py
+++ sources/models.py
@@ -49,3 +49,43 @@ class Parcel(BaseModel):
             return self.name
         else:
             return f'{self.owner}-parcel-{self.pk}'
+
+
+class Driver(BaseModel):
+    """
+
+    """
+
+    name = models.CharField(max_length=128, blank=True)
+    address = models.CharField(max_length=64, blank=True)
+    email = models.EmailField(max_length=64, blank=True)
+    phone = models.CharField(max_length=16, blank=True)
+    web = models.CharField(max_length=128, blank=True)
+
+    class Meta:
+        verbose_name = 'Driver'
+        verbose_name_plural = 'Drivers'
+    
+    def __str__(self):
+        return self.name
+
+class Vehicle(BaseModel, models.Model):
+    """
+    """
+
+    plate = models.CharField(max_length=8, blank=True)
+    model = models.CharField(max_length=64, blank=True)
+    brand = models.CharField(max_length=20, blank=True)
+    details = models.CharField(max_length=128, blank=True)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Vehicle'
+        verbose_name_plural = 'Vehicles'
+
+    def __str__(self):
+        return self.plate
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+
