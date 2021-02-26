79410435642755bf3f17dc46f4179e198aca860f
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-02-26 02:24:36 -0500

    updated base model fields

--- main/models.py
+++ main/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    creation_date = models.DateTimeField(auto_now_add=True, null=True)
-    update_date = models.DateTimeField(auto_now=True)
+    creation_date = models.DateTimeField(verbose_name='Created', auto_now_add=True, null=True)
+    update_date = models.DateTimeField(verbose_name='Last modified', auto_now=True)
     is_active = models.BooleanField(null=True, default=True)
 
     class Meta:
