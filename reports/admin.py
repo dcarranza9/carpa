33b5c2f8faaa5c28f01ef0ad31151452f88d44f6
otoro90 <33351976+otoro90@users.noreply.github.com>
2021-03-03 16:16:04 -0500

    Added report class in model.py and admin.py (#138)

--- reports/admin.py
+++ reports/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
 # Register your models here.
+from main.admin import BaseModelAdmin
+from reports.models import Report
+
+
+@register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    readonly_fields = BaseModelAdmin.readonly_fields
+    fieldsets = (
+        (None, {
+            'fields': (BaseModelAdmin.readonly_fields,)
+        }),
+        ('Report', {
+            'fields': ('date', 'bunchBatch', 'block', 'lot')
+        })
+    )
+    list_display = ('id', 'bunchBatch', 'block', 'lot', 'creation_date', 'update_date')
+    list_filter = ('creation_date', 'is_active',) 
--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+# from django.db.models.signals import post_save
+from main.models import BaseModel
+from harvests.models import BunchBatch
 
-# Create your models here.
+
+class Report(BaseModel):
+    """
+
+    """
+
+    date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    block = models.CharField(max_length = 16, blank=True)
+    lot = models.CharField(max_length = 16, blank=True)
+    bunchBatch = models.ForeignKey(BunchBatch, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = 'Report'
+        verbose_name_plural = 'Reports'
+
+    def __str__(self):
+        return f'Report-{self.bunchBatch.parcel.name}-{self.block}-{self.lot}'
+
