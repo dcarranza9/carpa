df5fdc3ecd9e4ca7fe539f38437594690c46dad6
rocandres <78989283+rocandres@users.noreply.github.com>
2021-03-05 15:55:35 -0500

    Test Bunchbatch model (#183)
    * added CRUD tests for BunchBatch model
    
    * added BunchBatch update test
    
    Co-authored-by: rocandres <rcandres@DESKTOP-D7BBAQH.localdomain>

--- harvests/tests/test_models.py
+++ harvests/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, tag
 from harvests.models import Bunch, BunchBatch, CategoryBunch, Harvester
+from sources.models import Parcel, Vehicle
 
 
 @tag('categories')
@@ -53,3 +54,49 @@ class TestHarvesters(TestCase):
     def test_delete_harvester(self):
         self.harvester.delete()
         self.assertNotIn(self.harvester, self.queryset)
+
+
+@tag('bunchbatch')
+class TestBunchBatch(TestCase):
+    fixtures = ['test/BunchBatch.json']
+
+    def setUp(self):
+        self.queryset = BunchBatch.objects.all()
+        self.bunchbatch= BunchBatch.objects.get(pk=10000)
+        self.parcel = Parcel.objects.get(pk=10000)
+        self.vehicle = Vehicle.objects.get(pk=10000)
+        self.bunch= Bunch.objects.get(pk=10000) 
+        self.bunch2= Bunch.objects.get(pk=10001)     
+        self.harvester = Harvester.objects.get(pk=10000)
+
+    def test_query_bunchbatch(self):
+        bunchbatch_status = self.bunchbatch.is_active
+        self.assertIs(bunchbatch_status, True)
+
+    def test_create_bunchbatch(self):
+        new_bunchbatch = BunchBatch.objects.create(            
+        	notes="test-create",
+        	delivery_time= "2021-03-01T17:00:00Z",
+        	parcel= self.parcel,
+        	vehicle= self.vehicle,        	
+        )
+        new_bunchbatch.bunches.add(self.bunch);
+        new_bunchbatch.harvesters.add(self.harvester);
+        self.assertEqual( new_bunchbatch.harvesters.get(pk=10000).name, 'Andres Cabanzo')
+
+    def test_update_bunchbatch(self):
+        self.bunchbatch.notes='test-update'
+        self.bunchbatch.save()
+        self.assertEqual(self.bunchbatch.notes, 'test-update')
+
+    def test_delete_bunchbatch(self):
+        self.bunchbatch.delete()
+        self.assertNotIn(self.bunchbatch, self.queryset)
+	
+
+
+    
+
+
+           
+
