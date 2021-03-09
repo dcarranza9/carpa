f957de27e75d2448e1a6dd1a39a9c1e03292fc33
DavidCarranza <dcarranza@hotmail.es>
2021-03-09 01:08:33 -0500

    Test for the Driver model #148 done

--- sources/tests/test_models.py
+++ sources/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, tag
 
-from sources.models import Parcel
+from sources.models import Parcel, Driver
 
 @tag('parcel')
 class TestParcel(TestCase):
@@ -31,3 +31,35 @@ class TestParcel(TestCase):
         self.parcel.delete()
         self.assertNotIn(self.parcel, self.queryset)
 
+
+@tag('driver')
+class TestParcel(TestCase):
+    fixtures = ['test/sourcesDriver.json']
+
+    def setUp(self):
+        self.queryset = Driver.objects.all()
+        self.driver = Driver.objects.get(pk=10000)
+
+    def test_query_driver(self):
+        driver_status = self.driver.is_active
+        self.assertEqual(driver_status, True)
+
+    def test_create_driver(self):
+        new_driver = Driver.objects.create(
+            is_active=True,
+            name='Señor X',
+            address='742 Evergreen Terrace',
+            email='amantedelacomida53@aol.com',
+            phone='0',
+            web='compumundohypermegared.net'
+        )
+        self.assertEqual(new_driver.name, 'Señor X')
+
+    def test_update_driver(self):
+        self.driver.name = 'El baron de la cerveza'
+        self.driver.save()
+        self.assertEqual(self.driver.name, 'El baron de la cerveza')
+
+    def test_delete_driver(self):
+        self.driver.delete()
+        self.assertNotIn(self.driver, self.queryset)
